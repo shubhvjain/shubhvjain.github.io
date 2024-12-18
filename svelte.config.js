bc5d92a5ffc8a569c24629437622d4148a84c920
shubh <shubhvjain@gmail.com>
2024-12-18 19:39:20 +0100

    neetlify setup

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,7 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-netlify';
 
 export default {
-	kit: {
-		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: "index.html",
-			precompress: false,
-			strict: true
-		})
-	}
+  kit: {
+    adapter: adapter()
+  }
 };
